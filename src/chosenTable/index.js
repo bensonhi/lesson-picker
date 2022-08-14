fa1b9a273031c3aac1c923052836437c67b95e2a
Benson <s980245@bles.tp.edu.tw>
2022-08-15 03:19:18 +0800

    prettier

--- src/chosenTable/index.js
+++ src/chosenTable/index.js
@@ -3,65 +3,69 @@ import './index.css';
 
 
 class LessonTable extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-    render(){
-    const unchoose =this.props.unchoose
-    const choose =this.props.choose;
-    const chosen =this.props.chosen
-    let shownColumn=[0,1,2,5,6,7,8,16,21]
-    function fieldContent(spans){
-        return spans.map((item,index)=>{
-           
-            return <a key={"https://aps.ntut.edu.tw/course/tw/"+item.href} href={"https://aps.ntut.edu.tw/course/tw/"+item.href} target="_blank" >{item.text}</a>
-        })
-    }
-    function rowContent(item){
-        let initial
-        if(chosen.includes(item)){
-            initial=[<td key={"choose"}><button onClick={()=>{unchoose(item)}}>去掉這個</button></td>]
+    render() {
+        const unchoose = this.props.unchoose
+        const choose = this.props.choose;
+        const chosen = this.props.chosen
+        let shownColumn = [0, 1, 2, 5, 6, 7, 8, 16, 21]
+        function fieldContent(spans) {
+            return spans.map((item, index) => {
+
+                return <a key={"https://aps.ntut.edu.tw/course/tw/" + item.href} href={"https://aps.ntut.edu.tw/course/tw/" + item.href} target="_blank" >{item.text}</a>
+            })
         }
-        else{
-            initial=[<td key={"unchoose"}><button onClick={()=>{choose(item)}}>選這個</button></td>]
-        }
-        return Object.entries(item).reduce((acc,text,index)=>{
-            let array=[...acc]
-            if(shownColumn.includes(index)){
-                if( !Array.isArray(text[1]) && index!=0 && typeof text[1] !== 'object')
-                    array.push(<td key={text[0]}>{text[1]}</td>)
-                else if (Array.isArray(text[1])  && index!==26)
-                    array.push(<td key={text[0]}>{fieldContent(text[1])}</td>)
+        function rowContent(item) {
+            let initial
+            if (chosen.includes(item)) {
+                initial = [<td key={"choose"}><button onClick={() => { unchoose(item) }}>去掉這個</button></td>]
             }
-            return array
-        },initial)
-        
-    }
-    let content = chosen.map((item,index)=>{
-        return <tr key={item['_id']}>{rowContent(item)}</tr>
-    })
-    let tablehead=[]
-    if(typeof chosen[0] === 'object'){
-        tablehead = Object.entries(chosen[0]).reduce((acc,text,index)=>{
-                if(index!==0 && shownColumn.includes(index))
-                return [...acc,<th key={text[0]}>{text[0]}</th>]
+            else {
+                initial = [<td key={"unchoose"}><button onClick={() => { choose(item) }}>選這個</button></td>]
+            }
+            return Object.entries(item).reduce((acc, text, index) => {
+                let array = [...acc]
+                if (shownColumn.includes(index)) {
+                    if (!Array.isArray(text[1]) && index != 0 && typeof text[1] !== 'object')
+                        array.push(<td key={text[0]}>{text[1]}</td>)
+                    else if (Array.isArray(text[1]) && index !== 26)
+                        array.push(<td key={text[0]}>{fieldContent(text[1])}</td>)
+                }
+                return array
+            }, initial)
+
+        }
+        let content = chosen.map((item, index) => {
+            return <tr key={item['_id']}>{rowContent(item)}</tr>
+        })
+        let tablehead = []
+        if (typeof chosen[0] === 'object') {
+            tablehead = Object.entries(chosen[0]).reduce((acc, text, index) => {
+                if (index !== 0 && shownColumn.includes(index))
+                    return [...acc, <th key={text[0]}>{text[0]}</th>]
                 return acc
-            },[<th key={"blank"}></th>])
+            }, [<th key={"blank"}></th>])
+        }
+        return (
+            <div>
+                <h2>已選課程</h2>
+                <table className='chosenTable'>
+                    <tbody>
+                        <tr key={"heading"}>
+                            {tablehead}
+                        </tr>
+                        {content}
+                    </tbody>
+                </table>
+                <a
+                    href={this.state.fileDownloadUrl}
+                    ref={e => this.dofileDownload = e}
+                >download it</a>
+            </div>
+        );
     }
-    return(
-        <div>
-        <h2>已選課程</h2>
-        <table className='chosenTable'>
-            <tbody>
-                <tr key={"heading"}>
-                    {tablehead}
-                </tr>
-                {content}
-            </tbody>
-        </table>
-        </div>
-      );
-    }
-  }
+}
 
 export default LessonTable;
